4e162a6bb8ae92eabdde64bd27ef858b50d722e8
hbiao68@yeah.net <hbiao68@yeah.net>
2015-05-21 16:00:10 +0800

    init

---
+++ src/com/shishuo/cms/action/Test.java
@@ -0,0 +1,12 @@
+package com.shishuo.cms.action;
+
+import com.shishuo.cms.util.AuthUtils;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(AuthUtils.getPassword("123456"));;
+	}
+
+}
